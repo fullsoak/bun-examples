eddb8d93eb1b52fe218783103f72212718dbe566
dk <hi@mrkha.ng>
2025-03-05 03:30:28 +0100

    upgraded to fullsoak@0.16.2

--- tests/MyTsxComponent.test.ts
+++ tests/MyTsxComponent.test.ts
@@ -1,8 +1,22 @@
-import { ssr } from "@fullsoak/fullsoak";
+import { useFullSoak } from "@fullsoak/fullsoak/testing";
+import { Controller, Get, ssr } from "@fullsoak/fullsoak";
+import { makeHat } from "@fullsoak/fullsoak/batteries";
 import { MyComponent } from "../src/components/MyComponent/index.tsx";
 import { expect, test } from "bun:test";
 
+@Controller()
+class MyController {
+  @Get("/")
+  serve() {
+    return ssr(MyComponent, { foo: "bar" }, {
+      headContent: makeHat({ title: "FullSoak on Oven 🔥" }),
+    });
+  }
+}
+
 test("MyTsxComponent", async () => {
-  const output = await ssr(MyComponent, { foo: "bar" });
+  const app = await useFullSoak({ controllers: [MyController] });
+  const resp = await app.fetch(new Request("http://localhost/"));
+  const output = await resp.text();
   expect(output).toMatchSnapshot();
 });
